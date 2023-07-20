4ae608489f28947e16521d1f9330c77bc9913383
Greg Albrecht <github.com@undef.net>
2023-07-20 15:42:10 -0700

    fix for ci

--- adsbcot/__init__.py
+++ adsbcot/__init__.py
@@ -20,7 +20,7 @@
 :source: <https://github.com/snstac/adsbcot>
 """
 
-__version__ = "6.1.0"
+__version__ = "6.1.1"
 __author__ = "Greg Albrecht <gba@snstac.com>"
 __copyright__ = "Copyright 2023 Sensors & Signals LLC"
 __license__ = "Apache License, Version 2.0"
