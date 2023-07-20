8e7b8d309db7271d6207110bf8083dbcd5a86284
Greg Albrecht <github.com@undef.net>
2023-07-20 16:47:53 -0700

    version bump

--- adsbcot/__init__.py
+++ adsbcot/__init__.py
@@ -20,7 +20,7 @@
 :source: <https://github.com/snstac/adsbcot>
 """
 
-__version__ = "6.2.0-beta3"
+__version__ = "6.2.0-beta4"
 __author__ = "Greg Albrecht <gba@snstac.com>"
 __copyright__ = "Copyright 2023 Sensors & Signals LLC"
 __license__ = "Apache License, Version 2.0"
